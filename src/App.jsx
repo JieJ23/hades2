e21a4e1163da071ffcfd5b083f1eba78e030e15f
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-05-04 02:12:52 -0400

    P8

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,82 @@ import Head from "./Comp/Head";
 import SideNav from "./Comp/Sidebar";
 import { h2Data } from "./Data/H2Data";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function sToA(string) {
   const array = string.split(`,`);
   return array;
 }
 
-const currentData = h2Data.sort((a, b) => (new Date(a.d) > new Date(b.d) ? -1 : 1));
-
 function App() {
+  const [category, setCategory] = useState(0);
+  const [sub, setSub] = useState(0);
+
+  const arrayData = [];
+  const aspectArrayData = [];
+
+  const customCategory = [`Latest`, `Time`];
+  const allAvailableAspects = [...new Set(h2Data.map((obj) => obj.a))].sort();
+
+  const handleChangeCategory = (num) => {
+    setCategory(num);
+  };
+  const handleChangeAspect = (num) => {
+    setSub(num);
+  };
+  //
+  const currentData = h2Data.slice().sort((a, b) => (new Date(a.d) > new Date(b.d) ? -1 : 1));
+  const timeData = h2Data.slice().sort((a, b) => (a.t > b.t ? 1 : -1));
+  arrayData.push(currentData);
+  arrayData.push(timeData);
+  //
+  const allAspectData = arrayData[category].filter((obj) => obj.a);
+  aspectArrayData.push(allAspectData);
+  for (let i = 0; i < allAvailableAspects.length; i++) {
+    const aspectData = arrayData[category].filter((obj) => obj.a === allAvailableAspects[i]);
+    aspectArrayData.push(aspectData);
+  }
+  //
+
+  const currentDisplay = aspectArrayData[sub];
+
   return (
     <main className="h-full min-h-lvh">
       <Head />
       <div className="flex flex-col md:flex-row gap-1 max-w-[1400px] mx-auto">
         <SideNav />
         <div className="p-1 w-full overflow-hidden">
+          <div className="p-1 flex gap-1 font-[PT]">
+            {customCategory.map((ite, index) => (
+              <button
+                class={`btn btn-sm ${category == index ? `btn-warning` : `btn-secondary btn-soft`}`}
+                onClick={() => handleChangeCategory(index)}
+              >
+                {ite}
+              </button>
+            ))}
+          </div>
+          <div className="flex border-1 border-white/20 p-1 font-[PT] text-[12px] my-0.5 bg-[#00000050] gap-1 overflow-x-scroll w-full rounded-sm">
+            <button
+              onClick={() => handleChangeAspect(0)}
+              class={`btn btn-sm ${sub == 0 ? `btn-warning` : `btn-secondary btn-soft`}`}
+            >
+              All
+            </button>
+            {allAvailableAspects.map((ite, index) => (
+              <button
+                onClick={() => handleChangeAspect(index + 1)}
+                class={`btn btn-sm ${sub == 1 + index ? `btn-warning` : `btn-secondary btn-soft`}`}
+              >
+                {ite}
+              </button>
+            ))}
+          </div>
           <div className="overflow-x-scroll">
             <table className="table select-none table-zebra">
               <thead>
                 <tr className="font-[Cinzel]">
-                  <th className="text-[10px] text-white">Latest</th>
+                  <th></th>
                   <th></th>
                   <th className="min-w-[180px]"></th>
                   <th className="min-w-[100px]"></th>
@@ -30,7 +86,7 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                {currentData.map((obj, index) => (
+                {currentDisplay.map((obj, index) => (
                   <tr key={index} className="font-[PT] text-[12px] overflow-hidden">
                     <td className="absolute">
                       <div className="w-8">
@@ -61,7 +117,7 @@ function App() {
                         <img draggable={false} className="size-8 rounded-none" src={`/H2Boons/${obj.fam}.png`} />
                         <div>
                           <div>{obj.fam}</div>
-                          <div className="opacity-60 text-[10px]">Familiar</div>
+                          <div className="opacity-80 text-[10px]">Patch {obj.p}</div>
                         </div>
                       </div>
                     </td>
