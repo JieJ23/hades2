8d9cfda98cb288ce7e6cdbd77b3782a7972a387a
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-06-04 15:37:00 -0400

    Homepage adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,13 @@ export default function App() {
     .sort((a, b) => (a.t > b.t ? 1 : -1))
     .sort((a, b) => (a.f > b.f ? -1 : 1))
     .filter((obj) => obj.src.includes(`youtub`))
-    .slice(0, 12);
+    .slice(0, 10);
 
   const displayContent2 = h1Data
     .slice()
-    .filter((obj) => obj.heat > 60)
-    .filter((obj) => obj.src.includes(`youtub`));
+    .sort((a, b) => (a.heat > b.heat ? -1 : 1))
+    .filter((obj) => obj.src.includes(`youtub`))
+    .slice(0, 10);
 
   return (
     <main className="h-full min-h-lvh select-none relative">
@@ -29,58 +30,43 @@ export default function App() {
         <SideNav />
         <div>
           <div className="mb-8">
-            <div className="px-4 text-[14px] text-white">* Fear Gameplay</div>
-            <section className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 p-2">
+            <div className="px-4 text-[16px] text-[#0fff7b] rounded font-[Cinzel]">Fear Gameplay</div>
+            <section className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 p-2">
               {displayContent.map((obj, index) => (
-                <div
-                  key={index}
-                  className="font-[PT] text-[12px] bg-black/50 border-1 border-white/10 rounded overflow-hidden"
-                >
+                <div key={index} className="font-[PT] text-[12px] rounded overflow-hidden">
                   <Link to={`${obj.src}`} target="_blank">
                     <img
                       src={`https://img.youtube.com/vi/${getYTid(obj.src)}/mqdefault.jpg`}
                       alt="Video"
+                      className="border-1 border-white/20 rounded"
                       draggable={false}
                     />
                   </Link>
-                  <div className="px-2 pb-1">
-                    <div className="flex justify-between">
-                      <div>{obj.n}</div>
-                      <div>{`${obj.f} F`}</div>
+                  <div className="p-2 pt-1">
+                    <div className="line-clamp-1">
+                      {obj.n} - {obj.f} - {obj.a} - {obj.l}
                     </div>
-                    <div className="flex justify-between gap-2">
-                      <div>{obj.a}</div>
-                      <div>{obj.t} T</div>
-                    </div>
+                    <div>{obj.t}</div>
                   </div>
                 </div>
               ))}
             </section>
           </div>
           <div className="my-8">
-            <div className="px-4 text-[14px] text-white">* Heat Gameplay</div>
-            <section className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 p-2">
+            <div className="px-4 text-[16px] text-[#ff5e1f] rounded font-[Cinzel]">Heat Gameplay</div>
+            <section className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 p-2">
               {displayContent2.map((obj, index) => (
-                <div
-                  key={index}
-                  className="font-[PT] text-[12px] bg-black/50 border-1 border-white/10 rounded overflow-hidden"
-                >
+                <div key={index} className="font-[PT] text-[12px] rounded overflow-hidden">
                   <Link to={`${obj.src}`} target="_blank">
                     <img
                       src={`https://img.youtube.com/vi/${getYTid(obj.src)}/mqdefault.jpg`}
                       alt="Video"
+                      className="border-1 border-white/20 rounded"
                       draggable={false}
                     />
                   </Link>
-                  <div className="px-2 pb-1">
-                    <div className="flex justify-between">
-                      <div>{obj.name}</div>
-                      <div>{`${obj.heat} H`}</div>
-                    </div>
-                    <div className="flex justify-between gap-2">
-                      <div>{obj.aspect}</div>
-                      <div>{obj.t} T</div>
-                    </div>
+                  <div className="pt-1 p-2 line-clamp-1">
+                    {obj.name} - {obj.heat} - {obj.aspect}
                   </div>
                 </div>
               ))}
