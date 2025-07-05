a54f0786df87463fb1192e4e101f7b5c9795f461
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-07-05 12:37:05 -0400

    Top 10

--- src/Page/Ranking.jsx
+++ src/Page/Ranking.jsx
@@ -4,52 +4,71 @@ import Background from "../Comp/Background";
 import { p9data } from "../Data/P9Data";
 import { findGUIcard } from "../App";
 import { parseTimetoms } from "../Data/Misc";
+import { weaponStaff, weaponAxe, weaponBlades, weaponCoat, weaponFlames, weaponSkull } from "../Data/Misc";
 
-const dataOrder = p9data
-  .slice()
-  .sort((a, b) => {
-    const feaDiff = +b.fea - +a.fea;
-    if (feaDiff !== 0) return feaDiff;
-    return parseTimetoms(a.tim) - parseTimetoms(b.tim);
-  })
-  .filter((obj) => obj.fea >= 50);
+const weaponType = [
+  `Melinoe Staff`,
+  `Melinoe Axe`,
+  `Melinoe Blades`,
+  `Melinoe Flames`,
+  `Melinoe Skull`,
+  `Melinoe Coat`,
+];
 
-const seen = new Set();
-const uniqueByNam = dataOrder.filter((item) => {
-  if (seen.has(item.nam)) return false;
-  seen.add(item.nam);
-  return true;
+const weaponCategory = [weaponStaff, weaponAxe, weaponBlades, weaponFlames, weaponSkull, weaponCoat];
+
+const dataOrder = p9data.slice().sort((a, b) => {
+  const feaDiff = +b.fea - +a.fea;
+  if (feaDiff !== 0) return feaDiff;
+  return parseTimetoms(a.tim) - parseTimetoms(b.tim);
 });
+// .filter((obj) => obj.fea >= 50);
+
+const fullData = [];
+
+for (let i = 0; i < weaponCategory.length; i++) {
+  const seen = new Set();
+  const uniqueByNam = dataOrder
+    .filter((obj) => weaponCategory[i].includes(obj.asp))
+    .filter((item) => {
+      if (seen.has(item.nam)) return false;
+      seen.add(item.nam);
+      return true;
+    });
+  fullData.push(uniqueByNam.slice(0, 10));
+}
 
 export default function Ranking() {
   return (
     <main className="relative">
       <Background />
       <Head />
-      <div className="max-w-[1200px] font-[PT] text-[14px] mx-auto text-white select-none">
+      <div className="max-w-[1200px] font-[PT] text-[12px] mx-auto text-white select-none">
         <SideNav />
-        <div className="p-4 pb-0 font-[Cinzel]">Fear 50+, Player's Best Overall</div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 p-2 pb-4 pt-0">
-          {uniqueByNam.map((obj, index) => (
-            <div
-              key={index}
-              className="min-h-[100px] md:min-h-[150px] rounded p-2 border-1 border-white/20 bg-[#00000090]"
-            >
-              <div className="font-[Cinzel] text-center text-[12px]">{obj.nam}</div>
-              <div className="flex items-center justify-center gap-2 w-full">
-                <img
-                  src={`/GUI_Card/c${findGUIcard(obj.asp)}.png`}
-                  alt="Aspect"
-                  className="w-[80px] rounded"
-                  draggable={false}
-                />
-                <div className="text-[12px]">
-                  <div>R: #{index + 1}</div>
-                  <div>A: {obj.asp}</div>
-                  <div>F: {obj.fea}</div>
-                  <div>T: {obj.tim}</div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  gap-2 p-2 pt-8">
+          {fullData.map((weaponData, index) => (
+            <div key={index} className="flex flex-col gap-2">
+              <img
+                src={`/GUI_Card/c${findGUIcard([weaponType[index]])}.png`}
+                alt="Aspect"
+                className="w-[80px] rounded mx-auto"
+                draggable={false}
+              />
+              {weaponData.map((item, idx) => (
+                <div
+                  key={idx}
+                  className="flex items-center gap-2 px-2 bg-[#0c0c20d0] border-1 border-black rounded py-1"
+                >
+                  <img
+                    src={`/P9/${item.asp}.png`}
+                    alt={item.asp}
+                    className="size-7 border-1 border-white/20 rounded-lg"
+                    draggable={false}
+                  />
+                  <span>{item.nam}</span>
+                  <span className="ml-auto">{item.fea}</span>
                 </div>
-              </div>
+              ))}
             </div>
           ))}
         </div>
