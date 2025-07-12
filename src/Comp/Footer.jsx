b128d3ded06688b28503348ba6d482a44e3cdb2c
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-07-12 09:10:20 -0400

    Added Entries

--- src/Comp/Footer.jsx
+++ src/Comp/Footer.jsx
@@ -1,10 +1,7 @@
 export default function Footer() {
   return (
     <div className="mt-6 pb-4 select-none flex flex-col text-gray-300 font-[Source] text-center text-[12px] px-2">
-      <div className="">This website has no affiliation with or endorsement from Supergaint Games, LLC.</div>
-      <div>
-        The data in this resource covers a period of one month, up to July 17th, since the release of The Unseen patch.
-      </div>
+      <div className="">This website has no affiliation with or endorsement from Supergaint Games.</div>
     </div>
   );
 }
