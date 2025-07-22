398f7707984f5cca0b19c07f7ec00c807390919f
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-07-22 03:29:10 -0400

    added ss entries

--- src/Comp/Footer.jsx
+++ src/Comp/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="mt-6 pb-4 select-none flex flex-col text-gray-300 font-[Source] text-center text-[12px] px-2">
+    <div className="mt-6 pb-4 select-none flex flex-col text-gray-300 font-[Source] text-center text-[11px] px-2">
       <div className="">This website has no affiliation with or endorsement from Supergiant Games.</div>
     </div>
   );
--- src/Page/Savestate.jsx
+++ src/Page/Savestate.jsx
@@ -19,28 +19,32 @@ const findBoss = (loc) => {
       return `Cerberus`;
     case `Erebus`:
       return `Hecate`;
+    case `Olympus`:
+      return `Prometheus`;
   }
 };
 
 export default function Savestate() {
+  const displayData = savestates.sort((a, b) => b.fea - a.fea);
+
   return (
     <main className="relative">
       <Background />
       <Head />
       <div className="max-w-[1200px] font-[Source] text-[12px] md:text-[13px] mx-auto">
         <SideNav />
-        <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 md:gap-2 p-1">
-          {savestates.map((obj, index) => (
+        <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 md:gap-2 p-1 px-2">
+          {displayData.map((obj, index) => (
             <div
               key={index}
-              className="border-1 border-black rounded bg-[#ffffff30] flex flex-col justify-between gap-1 p-1"
+              className="border-1 border-black rounded bg-[#a3a3a330] flex flex-col justify-between gap-1 gap p-1"
             >
               <div className="flex flex-wrap gap-1 text-[11px]">
                 <div className="px-2 py-1 bg-[#00ffaa] text-black rounded">{obj.share}</div>
-                <div className="px-2 py-1 bg-[#28282b] rounded">{obj.asp}</div>
-                <div className="px-2 py-1 bg-[#28282b] rounded">{obj.fam}</div>
-                <div className="px-2 py-1 bg-[#28282b] rounded">{obj.loc}</div>
-                <div className="px-2 py-1 bg-[#28282b] rounded">{obj.ks}</div>
+                <div className="px-2 py-1 bg-[#00ffaa] text-black rounded">{obj.asp}</div>
+                <div className="px-2 py-1 bg-[#131111] rounded">{obj.fam}</div>
+                <div className="px-2 py-1 bg-[#131111] rounded">{obj.loc}</div>
+                <div className="px-2 py-1 bg-[#131111] rounded">{obj.ks}</div>
                 <div className="px-2 py-1 bg-[#f18043] text-black rounded">{obj.fea}</div>
               </div>
               <div>
@@ -87,11 +91,11 @@ export default function Savestate() {
                 <img src={`Enemy/Biome${obj.loc}.png`} alt="Biome" className="h-[100px]" />
                 <img src={`Enemy/${findBoss(obj.loc)}.png`} alt="Biome" className="h-[100px]" />
               </div>
-              <div className="flex flex-wrap gap-1 text-[10px]">
+              <div className="flex flex-wrap gap-1 gap-y-0.5 text-[10px]">
                 {obj.arcana &&
                   deCodeArcana(obj.arcana)
                     .map((ite) => deckMatch[ite])
-                    .map((ite) => <div className="px-1 py-0.5 bg-[#28282b] rounded">{ite}</div>)}
+                    .map((ite) => <div className="px-1 py-0.5 bg-[#131111] rounded">{ite}</div>)}
               </div>
               <div className="flex text-[11px] gap-1 w-full">
                 <Link
