49a692f3cb200cc560263662cc6f96cbc969b7d2
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-07-22 08:56:19 -0400

    Added ss entries

--- src/Page/Savestate.jsx
+++ src/Page/Savestate.jsx
@@ -33,11 +33,11 @@ export default function Savestate() {
       <Head />
       <div className="max-w-[1200px] font-[Source] text-[12px] md:text-[13px] mx-auto">
         <SideNav />
-        <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 md:gap-2 p-1 px-2">
+        <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-1 px-2">
           {displayData.map((obj, index) => (
             <div
               key={index}
-              className="border-1 border-black rounded bg-[#a3a3a330] flex flex-col justify-between gap-1 gap p-1"
+              className="border-1 border-[black] rounded bg-[#28282bb5] flex flex-col justify-between gap-1 gap p-1"
             >
               <div className="flex flex-wrap gap-1 text-[11px]">
                 <div className="px-2 py-1 bg-[#00ffaa] text-black rounded">{obj.share}</div>
