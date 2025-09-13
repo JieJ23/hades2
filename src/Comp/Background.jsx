b684236ce48b5419e572106e7713980034fb40bf
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2025-09-13 12:17:57 -0400

    Updated BG

--- src/Comp/Background.jsx
+++ src/Comp/Background.jsx
@@ -1,7 +1,7 @@
 export default function Background() {
   return (
     <div className="fixed w-full h-lvh -z-10">
-      <div className="w-full h-full bg-[url('/Misc/mel1.webp')] bg-top bg-cover opacity-60" />
+      <div className="w-full h-full bg-[url('/Misc/v1.webp')] bg-top bg-cover opacity-40" />
       <div className="absolute top-0 w-full h-full bg-gradient-to-b to-[#131111] from-[#131111b1] pointer-events-none" />
     </div>
   );
