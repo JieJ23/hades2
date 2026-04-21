7eb87df91ec138b3ea6ca38da7da59fd7a3bb59f
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2026-04-21 16:20:49 -0400

    Added Images

--- src/Hook/Loading.jsx
+++ src/Hook/Loading.jsx
@@ -1,8 +1,9 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center gap-2 pt-10 font-[Ale]">
-      <span className="loading loading-ring loading-lg" />
-      <div className="text-white text-[20px] ">Loading</div>
+    <div className="flex justify-center items-center gap-2 pt-10 h-100">
+      <img src="load.png" alt="Charon Loader" className="w-20 h-20" />
+      <div className="text-white text-[20px] font-[Exo]">Loading</div>
+      <img src="load2.png" alt="Charon Loader" className="w-20 h-20" />
     </div>
   );
 }
