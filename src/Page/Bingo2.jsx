a7951517280db42b1be5dabd85202155a756cb0f
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2026-06-25 16:14:07 -0400

    Bingo Git: Size

--- src/Page/Bingo2.jsx
+++ src/Page/Bingo2.jsx
@@ -469,7 +469,7 @@ export default function Bingo2() {
         <div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 text-[14px]">
           {bingoSummary.map((obj, index) => (
             <div
-              className="min-h-75 rounded flex flex-col relative"
+              className="min-w-90 sm:min-w-75 min-h-75 rounded flex flex-col relative"
               style={{
                 borderStyle: "solid", // Required
                 borderWidth: "10px",
