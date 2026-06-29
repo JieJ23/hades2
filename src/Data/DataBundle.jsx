bd7cfb8ee12e63cf8421b206c94833b060c1e23e
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2026-06-29 09:20:14 -0400

    Added V4.data

--- src/Data/DataBundle.jsx
+++ src/Data/DataBundle.jsx
@@ -4,7 +4,8 @@ import { p11data } from "./P11Data";
 import { v1data } from "./V1data";
 import { v2data } from "./V2data";
 import { v3data } from "./V3data";
+import { v4data } from "./V4data";
 
-export const bundleData = [...p9data, ...p11data, ...v1data, ...v2data, ...v3data, ...p17data];
-export const v1bundle = [...v1data, ...v2data, ...v3data];
+export const bundleData = [...p9data, ...p11data, ...v1data, ...v2data, ...v3data, ...v4data, ...p17data];
+export const v1bundle = [...v1data, ...v2data, ...v3data, ...v4data];
 export const eabundle = [...p9data, ...p11data];
