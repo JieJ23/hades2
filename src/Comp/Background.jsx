fd68142289305401333fbc5bd5bcc4d4f8eb7fc0
JieJ23 <123914679+JieJ23@users.noreply.github.com>
2026-07-23 20:31:50 -0400

    Changed Wallpaper

--- src/Comp/Background.jsx
+++ src/Comp/Background.jsx
@@ -1,9 +1,9 @@
 export default function Background() {
   return (
     <div className="fixed w-full h-lvh -z-10 pointer-events-none ">
-      <div className="absolute w-full h-full bg-[url('/bio.webp')] bg-center bg-cover -z-10" />
-      <div className="absolute bottom-0 w-full h-[50vh] bg-linear-to-t from-black/90 via-black/85 to-black/80 z-40" />
-      <div className="absolute top-0 w-full h-[50vh] bg-linear-to-b from-black/90 via-black/85 to-black/80 z-40" />
+      <div className="absolute w-full h-full bg-[url('/home.webp')] bg-center bg-cover -z-10" />
+      <div className="absolute bottom-0 w-full h-[50vh] bg-linear-to-t from-[#0e0c12]/90 to-transparent z-40" />
+      <div className="absolute top-0 w-full h-[50vh] bg-linear-to-b from-[#0e0c12]/90 to-transparent z-40" />
     </div>
   );
 }
